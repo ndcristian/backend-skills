009a5a66a64912e80ba6addf3242001c6656daac
Cristian Nicolae <cristian.nicolae@quest-global.com>
2022-11-01 08:52:49 +0200

    first commit. init project

---
+++ src/test/java/com/etixapp/backendskills/BackendSkillsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etixapp.backendskills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSkillsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
