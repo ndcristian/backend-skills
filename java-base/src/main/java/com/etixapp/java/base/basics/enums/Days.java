ecb1c4e4151f08819d616d94647f74d65c435b14
Cristian Nicolae <cristian.nicolae@quest-global.com>
2024-08-06 09:47:16 +0300

    enums

---
+++ java-base/src/main/java/com/etixapp/java/base/basics/enums/Days.java
@@ -0,0 +1,6 @@
+package com.etixapp.java.base.basics.enums;
+
+public enum Days {
+	SUNDAY, MONDAY, TUESDAY, WEDNESDAY,
+	THURSDAY, FRIDAY, SATURDAY
+}
---
+++ java-base/src/test/java/enums/TestEnums.java
@@ -0,0 +1,15 @@
+package enums;
+
+import org.junit.Test;
+
+import com.etixapp.java.base.basics.enums.Days;
+
+public class TestEnums {
+
+	@Test
+	public void runTest(){
+		Days day = Days.MONDAY;
+		String dayTo = Days.MONDAY.name();
+
+	}
+}
